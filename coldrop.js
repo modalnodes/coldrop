2c8c12187a3beaea2d39939e9eb275602962ebb0
patonz91 <patonz91@gmail.com>
2016-03-17 21:53:57 +0100

    v0.1.0 Caldron, good luck

--- coldrop.js
+++ coldrop.js
@@ -1,47 +1,108 @@
+/* packages */
 var raspi = require('raspi-io');
+
 var five = require('johnny-five');
+var CronJob = require('cron').CronJob;
+
+var config = require('./res/config');
+
+
+var fs = require('fs');
+var obj = JSON.parse(fs.readFileSync('./res/config.json', 'utf8'));
+console.log(obj);
+/* pins  wiringPi*/
+var pin_boiler_try = 2
+var pin_boiler_real = 7
+
+var temp, bar, boiler_try, boiler;
+
 var board = new five.Board({
   io: new raspi()
 });
 
+
+
 board.on('ready', function() {
 
-var temp = new five.Temperature({
-  controller: "BMP180",
-  freq: 5000
-var alt = new five.Altimeter({
-  controller: "BMP180",
-  freq: 5000
-});
-
-temp.on("data", function() {
-    console.log("celsius: %d", this.C);
-    console.log("fahrenheit: %d", this.F);
-    console.log("kelvin: %d", this.K);
+  temp = new five.Temperature({
+    controller: "BMP180",
+    freq: 5000
   });
 
-var alt = new five.Altimeter({
-  controller: "BMP180",
-  freq: "5000"
+  temp.on("data", function(data, err) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    /*  console.log("celsius: %d", data.C);
+      console.log("fahrenheit: %d", data.F);
+      console.log("kelvin: %d", data.K);
+      console.log("err: %d", err);*/
+  });
+
+  /*var alt = new five.Altimeter({
+      controller: "BMP180",
+    });
+
+  alt.on("data", function(){
+   console.log("Altitude");
+      console.log("  feet   : ", this.feet);
+      console.log("  meters : ", this.meters);
+   console.log(" ");
+ });*/
+
+  bar = new five.Barometer({
+    controller: "BMP180",
+    freq: 5000
+  });
+
+  bar.on("data", function() {
+    /*  console.log("barometer");
+      console.log(" pressure : ", this.pressure);
+      console.log("-----------------------------------------");*/
+  });
+
+  /* "boiler" is the real relay connected  between rasp and the boiler */
+  boiler = new five.Relay(pin_boiler_real);
+
+
+  /* "boiler_try" is a free relay connected to rasp*/
+  boiler_try = new five.Relay(pin_boiler_try);
+
+  this.repl.inject({
+    boiler: boiler,
+    boiler_try: boiler_try
+  });
+
+
+
+
+
+
+
+
 });
 
-alt.on("data", function(){
- console.log("Altitude");
-    console.log("  feet   : ", this.feet);
-    console.log("  meters : ", this.meters);
- console.log(" ");
-});
-
-var bar = new five.Barometer({
-  controller: "BMP180",
-  freq: 5000
-});
-
-bar.on("data", function() {
-  console.log("barometer");
-  console.log(" pressure : ", this.pressure);
-  console.log("-----------------------------------------");
-});
 
 
+var boiler_on = new CronJob({
+  cronTime: '00 30 07 * * 1-5',
+  onTick: function() {
+    boiler.off();
+  },
+  start: false,
+  timeZone: 'Europe/Rome'
 });
+boiler_on.start();
+
+var boiler_off = new CronJob({
+  cronTime: '00 20 19 * * 1-5',
+  onTick: function() {
+    boiler.on();
+
+    console.log("boiler is off");
+  },
+  start: false,
+  timeZone: 'Europe/Rome'
+});
+boiler_off.start();
