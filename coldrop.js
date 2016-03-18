1a240957e9121bd211805657599f7760b5245209
Leonardo Montecchiari <patonz91@gmail.com>
2016-03-18 10:55:14 +0000

    coldrop.js edited online with Bitbucket: fixed Repl commands

--- coldrop.js
+++ coldrop.js
@@ -80,6 +80,8 @@ var boiler_on_saturday = new CronJob({
 cronTime: '00 12 00 * * 6',
 onTick: function(){
   boiler.off();
+  setTimeout(boiler.toggle(), 2000);
+  setTimeout(boiler.off(), 2000);
 },
 start:false,
 timeZone:'Europe/Rome'
@@ -89,7 +91,10 @@ boiler_on_saturday.start();
 var boiler_on = new CronJob({
   cronTime: '00 30 07 * * 1-5',
   onTick: function() {
-    boiler.off();
+  
+    setTimeout(boiler.toggle(), 2000);
+    setTimeout(boiler.off(), 2000);
+    setTimeout(boiler.off(), 2000);
   },
   start: false,
   timeZone: 'Europe/Rome'
@@ -100,7 +105,7 @@ var boiler_off = new CronJob({
   cronTime: '00 20 19 * * 1-5',
   onTick: function() {
     boiler.on();
-
+    setTimeout(boiler.on(), 2000);
     console.log("boiler is off");
   },
   start: false,
