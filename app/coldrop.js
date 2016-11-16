73f167afee62f5c64b6bd76815de1deb96faebad
patonz91 <patonz91@gmail.com>
2016-11-16 02:02:34 +0100

    setupping multiple devices

--- app/coldrop.js
+++ app/coldrop.js
@@ -1,34 +1,49 @@
 /* packages */
-var raspi = require('raspi-io');
 var five = require('johnny-five');
 var CronJob = require('cron').CronJob;
 var config = require('./res/config');
 var logger = require('./custom/logger');
 var fs = require('fs');
+var gpio = null;
+
+/* pins  wiringPi*/
+var pin_boiler_try = undefined;
+var pin_boiler_real = undefined;
+var temp, bar, boiler_try, boiler;
 
 var configFileName = 'config.json';
 
-logger.info("reading '"+configFileName+"'...");
-var obj = JSON.parse(fs.readFileSync('./res/'+configFileName, 'utf8'));
+logger.info("reading '" + configFileName + "'...");
+var obj = JSON.parse(fs.readFileSync('./res/' + configFileName, 'utf8'));
 logger.info("completed");
+
+switch (obj.config.device) {
+  case 'chip':
+  logger.info("chip device found on config--");
+    gpio = require('chip-io');
+    pin_boiler_try = 'XIO-P2';
+    pin_boiler_real = 'XIO-P0';
+    break;
+  case 'raspberry':
+  logger.info("raspberry device found on config--");
+    gpio = require('raspi-io');
+    pin_boiler_try = 7;
+    pin_boiler_real = 2;
+    break;
+  default:
+    logger.error("unknown device: check config.json and edit -device- key");
+}
 
 var cron_jobs = [];
 var currentProgram = undefined;
 var programActive = false;
 var overrideActive = false;
 
-/* pins  wiringPi*/
-var pin_boiler_try = 2;
-var pin_boiler_real = 7;
-var temp, bar, boiler_try, boiler;
-
-var board = new five.Board({
-  io: new raspi()
-});
-
 logger.info("setup the board");
-
-board.on('ready', function() {
+var board = new five.Board({
+  io: new gpio()
+});
+board.on('ready', function () {
 
 
 
@@ -52,25 +67,25 @@ board.on('ready', function() {
   boiler.off();
   logger.info("finished");
 
-  temp.on("change", function(data, err) {
+  temp.on("change", function (data, err) {
     if (err) {
       logger.error(err);
       return;
     }
-      logger.info("temperature changes: %d°", data.C);
-      logger.debug("celsius: %d", data.C);
-      logger.debug("fahrenheit: %d", data.F);
-      logger.debug("kelvin: %d", data.K);
+    logger.info("temperature changes: %d°", data.C);
+    logger.debug("celsius: %d", data.C);
+    logger.debug("fahrenheit: %d", data.F);
+    logger.debug("kelvin: %d", data.K);
 
-      if(currentProgram !== undefined && programActive !== false){ // check if exist a program
-        onChangeTemp(obj.config.program[currentProgram].temp, data.C, obj.config.program[currentProgram].tollerance);
-      } else if(overrideActive === true) { //check if override Mode is active
-        onChangeTemp(obj.config.override.temp, data.C, obj.config.override.tollerance);
-      } else { // or, in other case, shutdown the boiler
-        if(boiler.isOn){
-          boiler.off(); //
-        }
+    if (currentProgram !== undefined && programActive !== false) { // check if exist a program
+      onChangeTemp(obj.config.program[currentProgram].temp, data.C, obj.config.program[currentProgram].tollerance);
+    } else if (overrideActive === true) { //check if override Mode is active
+      onChangeTemp(obj.config.override.temp, data.C, obj.config.override.tollerance);
+    } else { // or, in other case, shutdown the boiler
+      if (boiler.isOn) {
+        boiler.off(); //
       }
+    }
 
   });
 
@@ -80,10 +95,11 @@ board.on('ready', function() {
     freq: 5000
   });
 
-  bar.on("data", function() {/*
-      console.log("barometer");
-      console.log(" pressure : ", this.pressure);
-      console.log("-----------------------------------------");*/
+  bar.on("data", function () {
+    /*
+          console.log("barometer");
+          console.log(" pressure : ", this.pressure);
+          console.log("-----------------------------------------");*/
   });
 
   this.repl.inject({
@@ -91,144 +107,152 @@ board.on('ready', function() {
     boiler_try: boiler_try
   });
 
-/*
-  var button = new five.Button(1);
+  /*
+    var button = new five.Button(1);
 
-  button.on("hold", function() {
-    console.log( "Button held" );
-  });
+    button.on("hold", function() {
+      console.log( "Button held" );
+    });
 
-  button.on("press", function() {
-    console.log( "Button pressed" );
-  });
+    button.on("press", function() {
+      console.log( "Button pressed" );
+    });
 
-  button.on("release", function() {
-    console.log( "Button released" );
-  });
-*/
-createChrono(boiler);
+    button.on("release", function() {
+      console.log( "Button released" );
+    });
+  */
+  createChrono(boiler);
 
-  fs.watch('./res/'+configFileName, (event, configFileName) => {
-    if (event==='change') {
+  fs.watch('./res/' + configFileName, (event, configFileName) => {
+    if (event === 'change') {
       currentProgram = undefined;
-      logger.info(configFileName+" change, realoading the program...");
+      logger.info(configFileName + " change, realoading the program...");
       logger.debug(cron_jobs.length);
-      for(var chrono of cron_jobs){
+      for (var chrono of cron_jobs) {
         chrono.stop();
       }
       cron_jobs = [];
-      fs.readFile('./res/'+configFileName, "utf8", function(err, data) {
+      fs.readFile('./res/' + configFileName, "utf8", function (err, data) {
         if (err) {
           logger.warning("try to save again the config.json file!");
           throw err;
         }
         obj = JSON.parse(data);
         createChrono(boiler);
-         logger.info("realoaded");
-    });
+        logger.info("realoaded");
+      });
     }
   });
 
 });
 
 
-function onChangeTemp(temp,currentTemp, tollerance){
+function onChangeTemp(temp, currentTemp, tollerance) {
   var deltaTemp = 0;
   deltaTemp = currentTemp - temp;
 
   if (deltaTemp > tollerance) {
-      if(!boiler.isOn){
-        logger.info("onChange: boiler is already OFF");
-      }else {
-        boiler.off();
-        logger.info("onChange: boiler is now OFF");
-      }
+    if (!boiler.isOn) {
+      logger.info("onChange: boiler is already OFF");
+    } else {
+      boiler.off();
+      logger.info("onChange: boiler is now OFF");
+    }
   }
   deltaTemp = temp - currentTemp;
   if (deltaTemp > tollerance) {
-      if(boiler.isOn){
-        logger.info("onChange: boiler is already ON");
-      }else {
-        boiler.on();
-        logger.info("onChange: boiler is now ON");
-      }
+    if (boiler.isOn) {
+      logger.info("onChange: boiler is already ON");
+    } else {
+      boiler.on();
+      logger.info("onChange: boiler is now ON");
+    }
   }
 
-  logger.info("onChange - boiler is ON: '"+boiler.isOn+"', with a temperature of %d°C", currentTemp);
+  logger.info("onChange - boiler is ON: '" + boiler.isOn + "', with a temperature of %d°C", currentTemp);
 
 
 }
 
-function createChrono(boiler){
-  if(obj.config.override.active === true){
-      overrideActive = true;
-      logger.info("override mode is active");
-      logger.info("temperature selected: %d", obj.config.override.temp);
-      logger.info("tollerance: %d", obj.config.override.tollerance);
-      if(boiler.isOn){
-        logger.info("override: boiler is already ON");
-      } else {
-        boiler.on();
-        logger.info("override: boiler is now ON");
-      }
+function createChrono(boiler) {
+  if (obj.config.override.active === true) {
+    overrideActive = true;
+    logger.info("override mode is active");
+    logger.info("temperature selected: %d", obj.config.override.temp);
+    logger.info("tollerance: %d", obj.config.override.tollerance);
+    if (boiler.isOn) {
+      logger.info("override: boiler is already ON");
+    } else {
+      boiler.on();
+      logger.info("override: boiler is now ON");
+    }
   } else {
+    if (boiler.isOff) {
+      logger.info("override: boiler is already OFF");
+    } else {
+      boiler.off();
+      logger.info("override: boiler is now OFF");
+    }
     logger.info("override mode is not active");
     logger.info("parsing 'config.json'...");
     var numberChrono = 0;
-    for (var item of obj.config.program){
+    for (var item of obj.config.program) {
       var numberProgram = clone(numberChrono);
-      var chrono_start = '00 '+item.start.minute+' '+item.start.hour+' * * '+item.day;
-      numberChrono = numberChrono +1
-      logger.info(numberChrono+" chrono start " + chrono_start);
+      var chrono_start = '00 ' + item.start.minute + ' ' + item.start.hour + ' * * ' + item.day;
+      numberChrono = numberChrono + 1
+      logger.info(numberChrono + " chrono start " + chrono_start);
 
-       cron_jobs.push(new CronJob({
-         cronTime: chrono_start,
-         onTick: function() {
-              programActive = true;
-              currentProgram = clone(numberProgram);
-              if(boiler.isOn){
-                logger.info("chrono: boiler is already ON");
-              } else {
-                boiler.on();
-                logger.info("chrono: boiler is now ON");
-              }
-         },
-         start: true,
-         timeZone: 'Europe/Rome'
-       }));
-
-
-
-    var chrono_end = '00 '+item.end.minute+' '+item.end.hour+' * * '+item.day;
-    logger.info(numberChrono + " chrono end " + chrono_end);
-
-     cron_jobs.push(new CronJob({
-       cronTime: chrono_end,
-       onTick: function() {
-            programActive = false;
-            if(!boiler.isOn){
-              logger.info("chrono: boiler is already OFF");
-            } else {
-              boiler.off();
-              logger.info("chrono: boiler is now OFF");
-            }
+      cron_jobs.push(new CronJob({
+        cronTime: chrono_start,
+        onTick: function () {
+          programActive = true;
+          currentProgram = clone(numberProgram);
+          if (boiler.isOn) {
+            logger.info("chrono: boiler is already ON");
+          } else {
+            boiler.on();
+            logger.info("chrono: boiler is now ON");
+          }
+        },
+        start: true,
+        timeZone: 'Europe/Rome'
+      }));
 
 
-       },
-       start: true,
-       timeZone: 'Europe/Rome'
-     }));
 
-   }
+      var chrono_end = '00 ' + item.end.minute + ' ' + item.end.hour + ' * * ' + item.day;
+      logger.info(numberChrono + " chrono end " + chrono_end);
 
- logger.info("parsing finished");
- }
+      cron_jobs.push(new CronJob({
+        cronTime: chrono_end,
+        onTick: function () {
+          programActive = false;
+          if (!boiler.isOn) {
+            logger.info("chrono: boiler is already OFF");
+          } else {
+            boiler.off();
+            logger.info("chrono: boiler is now OFF");
+          }
+
+
+        },
+        start: true,
+        timeZone: 'Europe/Rome'
+      }));
+
+    }
+
+    logger.info("parsing finished");
+  }
 }
 
-function clone(a) { return JSON.parse(JSON.stringify(a)); }
+function clone(a) {
+  return JSON.parse(JSON.stringify(a));
+}
 
 
-function chronoToDate(chrono){
+function chronoToDate(chrono) {
 
   //todo
 
